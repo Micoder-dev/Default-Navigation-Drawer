7929c288aff25e1061bbe3b73fa49810fe5bcb99
Micoder-dev <micoder.com@gmail.com>
2022-08-02 20:43:08 +0530

    initial push

---
+++ app/src/main/java/com/example/slidingrootnavigation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.slidingrootnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
